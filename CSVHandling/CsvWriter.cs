13e7109df429c50728267f3b46bf979501ea6059
Jesse <jesvis@bitconsult.be>
2021-04-16 09:57:49 +0200

    Add project files.

---
+++ CSVHandling/CsvWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSVHandling
+{
+    class CsvWriter
+    {
+        public static void writeCsvNoStreamWriter()
+        {
+            string filepath = @"";
+            StringBuilder output = new StringBuilder();
+            output.AppendLine("");
+            output.AppendLine("");
+
+            File.WriteAllText(filepath, output.ToString());
+            File.AppendAllText(filepath, output.ToString());
+        }
+        public static void writeCsvStreamWriter()
+        {
+            using (StreamWriter streamWriter = new StreamWriter(@""))
+            {
+                streamWriter.WriteLine("");
+                streamWriter.WriteLine("");
+            }
+        }
+    }
+}
---
+++ CSVHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSVHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
